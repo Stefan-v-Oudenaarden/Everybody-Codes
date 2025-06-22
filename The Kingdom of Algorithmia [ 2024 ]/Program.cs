bb50b30c3f123f593acfb6a38d5c31a8f7f79384
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-06-23 00:06:47 +0200

    2024 : Quest 1

--- The Kingdom of Algorithmia [ 2024 ]/Program.cs
+++ The Kingdom of Algorithmia [ 2024 ]/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using The_Kingdom_of_Algorithmia___2024__.Quest_01;
+
 Console.WriteLine("Hello, World!");
+
+var quest = new Quest01();
+quest.Solve();
